596847cba4e33ce9afdcd3216a321cac8d71ee1f
Mark Norwich <mna12@aol.com>
2014-10-11 18:40:05 -0400

    working

--- templates/BS.py
+++ templates/BS.py
@@ -2,22 +2,34 @@ import google
 from google import search
 import urllib 
 from bs4 import BeautifulSoup
-from sgmllib import SGMLParser
+
 
 r = []
 for url in search("who played batman?", tld='com', lang='en', stop = 10):
     r.append(url)
 
 
-#html = []
-#for x in r:
+url = r[0]
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
 
-sock = urllib.urlopen("http://www.biography.com/people/groups/actors-who-played-batman") 
-htmlSource = sock.read()  
-sock.close()       
-print htmlSource     
-    
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text.encode('utf-8'))
 
 
-    
-    
+
+
+   
