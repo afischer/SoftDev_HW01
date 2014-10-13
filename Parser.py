a877548bada6233956c10a8e9ed7cac4f5a6c0cb
Hubert <hubertpuszklewicz@gmail.com>
2014-10-12 23:46:18 -0400

    Iterable arguments now work

--- Parser.py
+++ Parser.py
@@ -3,18 +3,21 @@ from bs4 import BeautifulSoup
 
 #name parser, accepts a string or list of strings and returns a counter of the names. Later on I'll
 #probably replace the Counter with something easier to deal with.
-def findNames(text):
-    names = [ ]
+def findNames(text, iterating = False):
     if isinstance(text, collections.Iterable) and not isinstance(text, types.StringTypes):
+        names = []
         for elem in text:
-            names.extend(findNames(elem))
+            names.extend(findNames(elem, iterating = True))
+        return collections.Counter(names)
     if isinstance(text, str):
         d = enchant.Dict("en_us")
         pattern = "[^The]((([A-Z][a-z]+)|M([rs]|rs)\.|Dr\.)((\s[A-Z]\.)?\s[A-Z][a-z]+-?[A-Z]?[a-z]+)+)"
         result = [name[0] for name in re.findall(pattern, text)]
         inDict = lambda(word): any([d.check(part) for part in word.split(' ')])
-        names.extend([name for name in result if not inDict(name)])
-    return collections.Counter(names)
+        nameList = [name for name in result if not inDict(name)]
+        if iterating:
+            return nameList
+        return collections.Counter(nameList)
 
 #quick method to help get text from sites
 def getSites(url):
