4f98791a7eb569bb0b62ea9572525325f55cf60a
Hubert <hubertpuszklewicz@gmail.com>
2014-10-13 01:19:24 -0400

    Name parser complete

--- Parser.py
+++ Parser.py
@@ -1,14 +1,29 @@
 import enchant, re, collections, types, urllib
-from bs4 import BeautifulSoup
+from collections import Counter
+from operator import itemgetter 
 
-#name parser, accepts a string or list of strings and returns a counter of the names. Later on I'll
-#probably replace the Counter with something easier to deal with.
 def findNames(text, iterating = False):
+    """
+    Finds the frequency of full names in text.
+    Takes ~1.2 seconds to parse a string of length 6488634 (http://norvig.com/big.txt)
+
+    Args:
+      text (string): string object or iterable composed of strings
+      iterating (boolean, optional): changes depending on whether text is a string or iterable, do not change.
+
+    Returns:
+      List: made of tuples. First element of tuple is the name, second element is the frequency. The list is ordered by frequency.
+
+    Raises:
+      TypeError: If 'text' is not a string or iterable of strings.
+
+    """
+    convert = lambda(lst): sorted(list(Counter(lst).items()), key = itemgetter(1), reverse = True)
     if isinstance(text, collections.Iterable) and not isinstance(text, types.StringTypes):
         names = []
         for elem in text:
             names.extend(findNames(elem, iterating = True))
-        return collections.Counter(names)
+        return convert(names)
     if isinstance(text, str):
         d = enchant.Dict("en_us")
         pattern = "[^The]((([A-Z][a-z]+)|M([rs]|rs)\.|Dr\.)((\s[A-Z]\.)?\s[A-Z][a-z]+-?[A-Z]?[a-z]+)+)"
@@ -17,20 +32,7 @@ def findNames(text, iterating = False):
         nameList = [name for name in result if not inDict(name)]
         if iterating:
             return nameList
-        return collections.Counter(nameList)
-
-#quick method to help get text from sites
-def getSites(url):
-    bs = BeautifulSoup(urllib.urlopen(url))
-    return bs.get_text()
-
-if __name__ == '__main__':
-    test = str(getSites("http://en.wikipedia.org/wiki/Grigori_Perelman").encode('ascii', 'ignore').replace('\n', ''))
-    test2 = str(getSites("http://en.wikipedia.org/wiki/Grigori_Perelman").encode('ascii', 'ignore').replace('\n', ''))
-    testList = [test, test2]
-    print "\n\n"
-    print findNames(test)
-    print "\n\n"
-    print findNames(testList)
+        return convert(nameList)
+    raise TypeError("Input must a string or an iterable object composed of strings.")
 
     
