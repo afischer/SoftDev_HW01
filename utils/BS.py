7505f30d909832ec3e0dbb6644d12f3569f9dc8c
ryustegolob <ryustegolob@gmail.com>
2014-10-14 01:29:48 -0400

    cleaned up and workin

--- utils/BS.py
+++ utils/BS.py
@@ -1,16 +1,17 @@
-from Parser import altFindNames
+from Parser import altFindNames, findDates
 import google
 from google import search
 import urllib 
 from bs4 import BeautifulSoup
 
+def txt(question):
+    
+    r = []
+    for url in search(question, tld='com', lang='en', stop = 20):
+        r.append(url)
 
-r = []
-for url in search("When was the us created?", tld='com', lang='en', stop = 20):
-    r.append(url)
-
-def txt(r):
-    for x in r:
+    allText = ""
+    for x in r: #gets the urls
         url = x
         html = urllib.urlopen(url).read()
         soup = BeautifulSoup(html, "html5lib")
@@ -20,10 +21,24 @@ def txt(r):
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
         text = '\n'.join(chunk for chunk in chunks if chunk)
-        return text.encode('utf-8')
+        allText = allText + text.encode('utf-8') #this is all of the text in the websites
 
-print altFindNames(txt(r))
-#txt(r)
+    results= []
+    if question[:4] == "when":
+        results = findDates(allText)
+    else:
+        results = altFindNames(allText)
+    
+    final = results[0]
+    return final
+
+print txt("when was the us founded")
+
+
+
+
+   
+
 
 
 
